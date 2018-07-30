1a64ba80203049eeaa6427b54f891ebdd3ce9a52
arianna haradon <ariannaharadon@gmail.com>
2018-07-30 15:08:48 -0500

    add dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Header } from './web/Header';
 import { Main } from './web/Main';
-import { NavigationBar } from './web/NavigationBar';
 
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
-        <NavigationBar />
         <Main />
       </div>
     );
