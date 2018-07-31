1a523435cd1ce649f3d9b278ea17686d8ed7a902
arianna haradon <ariannaharadon@gmail.com>
2018-07-31 14:41:54 -0500

    add video and video responsiveness

--- src/web/Modal.js
+++ src/web/Modal.js
@@ -1,28 +1,9 @@
 import React from 'react';
-
+import video from './video.mp4'
 export const Modal = ({ content }) => (
   <div className='bg-brown modal'>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas erat sem,
-     convallis sed nisi sit amet, elementum varius mauris. Nunc finibus eleifend
-      tellus, vitae faucibus enim consectetur at. Vivamus sed finibus mi.
-      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-      posuere cubilia Curae; Mauris sed sem pretium erat tincidunt tempus.
-       Etiam at euismod massa. Vestibulum venenatis commodo odio at iaculis.
-        Aenean vel pellentesque mi. Sed aliquet lacus eget velit finibus
-        molestie. Suspendisse quis dui lorem. In sollicitudin, orci quis
-         porta volutpat, dolor ante mollis metus, vel dignissim erat dolor
-          id enim. Vestibulum pellentesque pellentesque dolor ac ultricies.
-          Maecenas accumsan, massa vel varius malesuada, turpis diam congue
-          enim, sed rutrum dui nisl vel dolor. Suspendisse vulputate nunc
-          risus, consequat egestas nisl posuere sed.
-
-          In quis justo eget nunc ultrices tincidunt eu sed mauris.
-           Nulla sodales laoreet magna quis efficitur. Donec posuere augue
-           ante, vitae posuere turpis varius et. Nunc vel purus ac lorem
-           consequat facilisis eget quis enim. Sed pretium in eros a
-           placerat. Sed quis ullamcorper ex. Duis ut mi diam. Etiam
-           gravida metus sit amet ex sodales tristique. Cras ornare urna
-           sit amet malesuada auctor. Etiam placerat feugiat lectus nec
-            maximus. Aenean ullamcorper ullamcorper dui at finibus.
+    <video className='video' autoPlay='true' controls>
+      <source src={video} type='video/mp4'/>
+    </video>
   </div>
 );
